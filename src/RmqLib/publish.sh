0806f1a2e52b070f6fdeb79b0be895b04f1248f5
Milovidov Vasya <milovidov.vasya@gmail.com>
2020-12-12 16:57:15 +0300

    pack info

---
+++ src/RmqLib/publish.sh
@@ -0,0 +1,8 @@
+﻿set -e;
+rm -rf bin
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release
+
+
+dotnet nuget push `ls ./bin/Release/*.nupkg`  --api-key SECRET_KEY --source https://api.nuget.org/v3/index.json
