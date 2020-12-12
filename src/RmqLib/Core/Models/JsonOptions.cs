bd923127429524e22672180737456214d8ab2527
Vasiliy Milovidov <vam@automama.ru>
2020-12-12 14:58:45 +0300

    add serialize options

---
+++ src/RmqLib/Core/Models/JsonOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace RmqLib.Core {
+	public class JsonOptions {
+		public static JsonSerializerOptions Default = new JsonSerializerOptions {
+			PropertyNameCaseInsensitive = true,
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			IgnoreNullValues = true
+		};
+	}
+}
