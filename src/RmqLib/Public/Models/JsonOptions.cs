f4eeeaaea7925649a2d947eaa18817fab36f141d
Milovidov Vasya <milovidov.vasya@gmail.com>
2020-12-14 21:17:54 +0300

    fix enum serialization

--- src/RmqLib/Public/Models/JsonOptions.cs
+++ src/RmqLib/Public/Models/JsonOptions.cs
@@ -1,11 +1,22 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace RmqLib {
 	public class JsonOptions {
-		public static JsonSerializerOptions Default = new JsonSerializerOptions {
-			PropertyNameCaseInsensitive = true,
-			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-			IgnoreNullValues = true
-		};
+		public static JsonSerializerOptions Default;
+
+		static JsonOptions() {
+			Default = new JsonSerializerOptions {
+				PropertyNameCaseInsensitive = true,
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				IgnoreNullValues = true,
+
+			};
+
+			Default.Converters.Add(new JsonStringEnumConverter());
+		}
 	}
+
+    
 }
